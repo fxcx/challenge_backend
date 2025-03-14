ebbd5ab8f222c3497915ce584afa73ce93483606
fxcx <facundo19burgos@gmail.com>
2025-03-14 00:55:48 -0300

    init proyect

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+// import swaggerJSDoc from "swagger-jsdoc"
+// import path from "path"
+import Express from 'express';
+const PORT = process.env.PORT ?? 5000;
+const app = Express();
+app.use(Express.json());
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
